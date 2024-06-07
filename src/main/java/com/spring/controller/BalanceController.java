ce3a482be7923d4294476090ec4bd83d0364f926
gangjaehyeog <xcvdk2397@gmail.com>
2024-06-07 17:03:39 +0900

    컨트롤러 추가

---
+++ src/main/java/com/spring/controller/BalanceController.java
@@ -0,0 +1,16 @@
+package com.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 유저의 거래내역과 잔고 정보 반환
+ */
+@RestController
+public class BalanceController {
+
+    @GetMapping("/myBalance")
+    public String myBalance() {
+        return "My Balance";
+    }
+}
